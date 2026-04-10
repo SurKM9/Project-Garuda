cbe6ec4a92322994c2e89820d11ca9b50ab6551e
Suryakiran Maruvada <msuryakiran09@outlook.com>
2026-04-10 17:33:32 +0200

    feat: Add tests

---
+++ Tests/test_main.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+#include "TelemetryData.hpp"
+
+
+/**
+ * @brief Logic Test: Ensure our safety checks catch low battery.
+ */
+TEST(SafetyLogicTests, BatterySafetyCheck) {
+    TelemetryPacket packet;
+
+    // case A: Battery high
+    packet.battery_pct = 95;
+    bool isSafe = (packet.battery_pct > 20);
+    EXPECT_TRUE(isSafe);
+
+    // case B: Battery low
+    packet.battery_pct = 15;
+    isSafe = (packet.battery_pct > 20);
+    EXPECT_FALSE(isSafe) << "Safety failed: 15% battery level is not flight ready";
+}
+
+/**
+ * @brief Ensure altitude is not negative
+ */
+TEST(SafetyLogicTests, AltitudeNegativeCheck){
+
+    TelemetryPacket packet;
+
+    packet.altitude = 10.5f;
+    EXPECT_GE(packet.altitude, 0.0f)
+        << "UAV reported an underground altitude! Check sensor calibration.";
+}
+
+/**
+ * @brief Test the memory alignment and size of our network packets.
+ * This ensures that 'packed' structures don't accidentally grow.
+ */
+TEST(ProtocolTests, PacketSizeCheck) {
+    // Current packet:
+    // uint32 (4) + 4x float (16) + uint8 (1) + uint8 (1) = 22 bytes
+    const size_t expected_size = 22;
+
+    EXPECT_EQ(sizeof(TelemetryPacket), expected_size)
+        << "CRITICAL: TelemetryPacket size mismatch! Network protocol will break.";
+}
+
+/**
+ * @brief Simple math test to ensure GTest is working correctly.
+ */
+TEST(SystemTests, GTestSanityCheck) {
+    int result = 2 + 2;
+    ASSERT_EQ(result, 4);
+}
